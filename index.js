e2255c8d41f7a83dfbfc6a124028fd08105cecf2
danielgiron <73446404+danielgiron@users.noreply.github.com>
2022-08-06 15:05:30 -0400

    first production build commit

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(
   cors({
-    httpsOnly: true,
+    httpsOnly: false,
     origin: "http://localhost:3000",
     // origin: "https://tasky-social-network.herokuapp.com",
     methods: ["POST", "PUT", "GET", "DELETE"],
